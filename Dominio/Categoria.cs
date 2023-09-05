8f21e926280a69c2f3b789123d73bc705d1d4943
nahuelRomero1 <naahuel96@hotmail.com>
2023-09-05 00:02:20 -0300

    Subiendo primera avance de clases

---
+++ Dominio/Categoria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+   public class Categoria
+    {
+        public Categoria()
+        {
+
+        }
+        public Categoria(int Id, string Desc)
+        {
+            Descripcion = Desc;
+            this.Id = Id;
+        }
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+    }
+}
